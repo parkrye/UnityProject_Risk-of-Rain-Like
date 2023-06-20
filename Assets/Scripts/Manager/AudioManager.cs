30d0189e51bd5eb12065ea674b9e8323b31c47b9
박경륜 <parkrye@gmail.com>
2023-06-20 21:06:13 +0900

    feat: 추후 확장을 위한 매니저 추가

---
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public float TotalVolume;
+    public float BGMVolume;
+    public float SEVolume;
+}
